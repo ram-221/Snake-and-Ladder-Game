41ee416d47713cbdfd1cf1ad65596fbcd6f6529e
ram-221 <rameshmalli857.com>
2022-07-13 16:57:24 +0530

    Die number get between 1 to 6

--- bridgelabday04UC/SnakeAndLadder.java
+++ bridgelabday04UC/SnakeAndLadder.java
@@ -1,11 +1,11 @@
 package com.bridgelabday04UC;
 
 public class SnakeAndLadder {
-
 	public static void main(String[] args) {
-
-		int startPosition = 0; 
-	    System.out.println("Game start and Player is at Position " +startPosition);
+		int startPosition = 0;
+		System.out.println("Game start and Player is at Position " +startPosition);
+		int dice = (int) Math.floor(Math.random() * 6 + 1);
+		System.out.println("The Number on dice is " + dice);
 	}
 
 }
