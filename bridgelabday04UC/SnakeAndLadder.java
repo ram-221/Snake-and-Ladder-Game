b09ee364e7a7981b7c5b12e536028245bb69d15e
ram-221 <rameshmalli857.com>
2022-07-13 17:02:32 +0530

    Player checks for a option

--- bridgelabday04UC/SnakeAndLadder.java
+++ bridgelabday04UC/SnakeAndLadder.java
@@ -1,11 +1,39 @@
 package com.bridgelabday04UC;
 
 public class SnakeAndLadder {
+
 	public static void main(String[] args) {
-		int startPosition = 0;
-		System.out.println("Game start and Player is at Position " +startPosition);
-		int dice = (int) Math.floor(Math.random() * 6 + 1);
-		System.out.println("The Number on dice is " + dice);
+		// TODO Auto-generated method stub
+
+		System.out.println("Starting of Snake and Ladder Game");
+		final int NoPlay = 0;
+		final int LADDER = 1;
+		final int SNAKE = 2;
+		//variables
+		int dice = (int)((Math.random()*10) % 6);
+		int position =(int)((Math.random()*10) %3 );
+		int playerCurrentPosition = 0;
+		switch(position)
+		{
+		case 0: 
+			playerCurrentPosition= NoPlay;
+			System.out.println("No Movement");
+			break;
+		case 1: 
+			playerCurrentPosition = LADDER;
+			playerCurrentPosition +=dice;
+			System.out.println("Ladder move forward " + playerCurrentPosition);
+			break;
+		case 2:
+			playerCurrentPosition =SNAKE ;
+			playerCurrentPosition -=dice;
+			System.out.println("Snakes move backward "+ playerCurrentPosition);
+			break;
+		default :
+			playerCurrentPosition +=dice;
+			System.out.println("Movement to " +playerCurrentPosition );
+		}
+
 	}
 
 }
