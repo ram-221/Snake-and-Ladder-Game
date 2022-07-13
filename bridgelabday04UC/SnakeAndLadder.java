dfd243b5ac65d225f9f3f236bafabef193532b9d
ram-221 <rameshmalli857.com>
2022-07-13 16:51:39 +0530

    Snake And Ladder Satrt Position 0

---
+++ bridgelabday04UC/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabday04UC;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+
+		int startPosition = 0; 
+	    System.out.println("Game start and Player is at Position " +startPosition);
+	}
+
+}
